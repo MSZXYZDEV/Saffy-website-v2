f526feca4ad65f4822cff5060950edf36cc2ae3d
zQr00 <46131582+zQr00@users.noreply.github.com>
2020-04-07 13:22:57 +0200

    stary układ

--- public/js/copyright.js
+++
@@ -1,5 +0,0 @@
-function copyright() {
-    const date = new Date();
-    const year = date.getFullYear();
-    document.getElementById('copyright').innerHTML = `Copyright &copy; ${year} by Saffy.pl`;
-}
